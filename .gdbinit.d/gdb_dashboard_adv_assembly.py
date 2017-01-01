5f3c48775a484b891f0e79bf53888629b463a4e9
Sharkz Wang <sharkz.wang@gmail.com>
2017-01-01 15:19:53 +0800

    GDB: enhanced asm. dashboard with break-points

--- .gdbinit.d/gdb_dashboard_adv_assembly.py
+++ .gdbinit.d/gdb_dashboard_adv_assembly.py
@@ -98,9 +98,16 @@ instructions constituting the current statement are marked, if available."""
                     func_info = '? '
             else:
                 func_info = ''
-            format_string = '{}{}{}{}{}'
+            format_string = '{}{}{}{}{}{}'
             indicator = ' '
             text = highlighter.process(text)
+
+            is_bp = self.is_break_point(int(addr_str, 16))
+            if not R.ansi:
+                bp_mark = "BP " if is_bp else "   "
+            else:
+                bp_mark = ansi("BP ", R.style_error) if is_bp else "   "
+
             if addr == frame.pc():
                 if not R.ansi:
                     indicator = '>'
@@ -120,9 +127,15 @@ instructions constituting the current statement are marked, if available."""
             else:
                 addr_str = ansi(addr_str, R.style_low)
                 func_info = ansi(func_info, R.style_low)
-            out.append(format_string.format(addr_str, indicator,
+            out.append(format_string.format(bp_mark, addr_str, indicator,
                                             opcodes, func_info, text))
         return out
+
+    def is_break_point(self, addr):
+        for bp in map(lambda bp: int(bp.location.replace("*", ""), 16), gdb.breakpoints()):
+            if addr == bp:
+                return True
+        return False
 
     def attributes(self):
         return {
